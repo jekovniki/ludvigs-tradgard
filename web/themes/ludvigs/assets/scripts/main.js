28a46e4b139e6fee2b42a0814067e4ff5d0440f5
jekovniki <jekovniki95@gmail.com>
2022-06-26 20:23:12 +0300

    Update js

--- web/themes/ludvigs/assets/scripts/main.js
+++ web/themes/ludvigs/assets/scripts/main.js
@@ -1,1 +1,16 @@
 // This is the main javascript file
+
+function setPlaceholderToForm() {
+    console.log(1);
+    const textfields = document.querySelectorAll('form .js-form-type-textfield');
+    const email = document.querySelectorAll('form .js-form-type-email');
+    const textarea = document.querySelectorAll('form .js-form-wrapper');
+
+    for (const item of textfields) {
+        const labels = item.querySelector('label');
+        console.log(labels);
+    }
+
+}
+
+setPlaceholderToForm();
